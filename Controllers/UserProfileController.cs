8261935859b2accacae646d044054996f125909b
thibs27 <ben_thibs@hotmail.ca>
2021-08-24 14:21:25 -0400

    Modified user profile controller

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -7,8 +7,6 @@ using System.Threading.Tasks;
 namespace net5backendwithidentityandADandGroups.Controllers
 .Controllers
 {
-    // This is how groups ids/names are used in the Authorize attribute
-    //[Authorize(Roles = "8873daa2-17af-4e72-973e-930c94ef7549")] 
     public class UserProfileController : Controller
     {
         private readonly GraphServiceClient graphServiceClient;
@@ -17,6 +15,7 @@ namespace net5backendwithidentityandADandGroups.Controllers
         {
             this.graphServiceClient = graphServiceClient;
         }
+
         [Authorize(Policy = "GroupAdmin")]
         [AuthorizeForScopes(Scopes = new[] { "User.Read" })]
         public async Task<IActionResult> Index()
@@ -24,15 +23,6 @@ namespace net5backendwithidentityandADandGroups.Controllers
             User me = await graphServiceClient.Me.Request().GetAsync();
             ViewData["Me"] = me;
 
-            try
-            {
-                var photo = await graphServiceClient.Me.Photo.Request().GetAsync();
-                ViewData["Photo"] = photo;
-            }
-            catch
-            {
-                //swallow
-            }
             return View();
         }
     }
